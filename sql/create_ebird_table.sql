dc6db08c41a53e8309b32b938860ba3f59365d06
@DornMoore <dorn@savingcranes.org>
2017-03-24 19:26:59 -0500

    Another comment

--- sql/create_ebird_table.sql
+++ sql/create_ebird_table.sql
@@ -1,9 +1,16 @@
-CREATE TABLE ebd.eBird (
-  GLOBAL_UNIQUE_IDENTIFIER    char(50),
+--
+-- eBird Data import
+--
+-- Create the inital table of eBird Data. 
+--  this table created from a modified version of 
+--  information found here: https://github.com/weecology/retriever/issues/90
+--
+CREATE TABLE ebd.ebird (
+  GLOBAL_UNIQUE_IDENTIFIER      char(50), 
     LAST_EDITED_DATE            date,
     TAXONOMIC_ORDER             numeric,
-    CATEGORY                    varchar(20),
-    COMMON_NAME                 varchar(70),
+    CATEGORY                    varchar(20),    
+    COMMON_NAME                 varchar(70),  
     SCIENTIFIC_NAME             varchar(70),
     SUBSPECIES_COMMON_NAME      varchar(70),
     SUBSPECIES_SCIENTIFIC_NAME  varchar(70),
@@ -48,17 +55,26 @@ CREATE TABLE ebd.eBird (
     X                           text
 );
 
+
+-- Import the data that you downloaded from eBird
+-- See sample_copy_ebird_data.sql file for details
+
+-- Wait to do the steps below until all of the data has been imported. 
+
 -- Create Indexes
-----
-CREATE INDEX ebird_common_name_gix ON "ebd"."ebird" USING BTREE (common_name);
-CREATE INDEX ebird_geom_gix ON "ebd"."ebird" USING GIST (geom);
+-----------------
+
+-- Index for common_name, used lower() to make case insensitive searchers easier 
+CREATE INDEX ebird_common_name_idx ON "ebd"."ebird" ((lower(common_name)));
+-- Index for the GUID although the GUID isn't actually unique...
+CREATE INDEX ebird_global_unique_identifier_idx ON "ebd"."ebird" (global_unique_identifier);
 
 -- Get the Spatial Data Ready
 -------------------------------
 --Add the geom field for holding spatial data
 ALTER TABLE "ebd"."ebird" ADD COLUMN geom geometry(Geometry,4326);
 
---Add the spatial dat to the new column
+--Add the spatial data to the new column
 UPDATE "ebd"."ebird"
 set geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);
 WHERE geom IS NULL;
@@ -66,7 +82,13 @@ WHERE geom IS NULL;
 -- Create Index on the geom field
 CREATE INDEX ebird_geom_gix ON "ebd"."ebird" USING GIST (geom);
 
--- Cleanup and optimize the data
-VACUUM ANALYZE "ebd"."ebird";
+-- Cleanup the data table.
+VACUUM "ebd"."ebird";
 
+--Since our data will be primarily used for spatial queries
+--  I've going to cluser records based on their location.
+CLUSTER "ebd"."ebird" USING ebird_geom_gix;
 
+-- Finally, I'll ask the database to update teh statistics 
+--  vacuuming is unnecessary since it was done above.
+ANALYZE "ebd"."ebird";
